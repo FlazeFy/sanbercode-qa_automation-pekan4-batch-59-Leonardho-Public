75d55762fc1b1d7f7c4e386a7a26d0aa5db55b4d
Leonardho Rante S <newbie8801@gmail.com>
2024-10-05 08:09:05 +0700

    Auth module test E2E

---
+++ support/e2e/auth_module.js
@@ -0,0 +1,89 @@
+describe('Auth Module', () => {
+    it('TC-AU-001 - User can login to Swag Labs with valid credentials (Positive)', () => {
+        // Test Data
+        const username = "standard_user"
+        const password = "secret_sauce"
+
+        // Step 1 : User visit the base url
+        cy.visit('/')
+
+        // Step 2 : User fill the username and password field
+        cy.get('#user-name').should('exist').type(username)
+        cy.get('#password').should('exist').type(password)
+
+        // Step 3 : User press the "Login" button
+        cy.get('#login-button').should('exist').click()
+
+        // Expected Result
+        cy.url().should('include', '/inventory.html') 
+
+        // Audit
+        cy.screenshot('TC-AU-001 - Expected Result')
+    })
+
+    it('TC-AU-002 - User can not login to Swag Labs with empty username (Negative)', () => {
+        // Test Data
+        const password = "secret_sauce"
+
+        // Step 1 : User visit the base url
+        cy.visit('/')
+
+        // Step 2 : User fill the password field
+        cy.get('#password').should('exist').type(password)
+
+        // Step 3 : User press the "Login" button
+        cy.get('#login-button').should('exist').click()
+
+        // Expected Result
+        cy.url().should('include', '/') 
+        cy.get('.error-message-container').should('exist').and('contain.text', 'Username is required') 
+
+        // Audit
+        cy.screenshot('TC-AU-002 - Expected Result')
+    })
+
+    it('TC-AU-003 - User can not login to Swag Labs with empty password (Negative)', () => {
+        // Test Data
+        const username = "standard_user"
+
+        // Step 1 : User visit the base url
+        cy.visit('/')
+
+        // Step 2 : User fill the username field
+        cy.get('#user-name').should('exist').type(username)
+
+        // Step 3 : User press the "Login" button
+        cy.get('#login-button').should('exist').click()
+
+        // Expected Result
+        cy.url().should('include', '/') 
+        cy.get('.error-message-container').should('exist').and('contain.text', 'Password is required')
+
+        // Audit
+        cy.screenshot('TC-AU-003 - Expected Result') 
+    })
+
+    it('TC-AU-004 - User can login to Swag Labs with valid credentials (Negative)', () => {
+        // Test Data
+        const username = "special_user"
+        const password = "spicy_sauce"
+
+        // Step 1 : User visit the base url
+        cy.visit('/')
+
+        // Step 2 : User fill the username and password field
+        cy.get('#user-name').should('exist').type(username)
+        cy.get('#password').should('exist').type(password)
+
+        // Step 3 : User press the "Login" button
+        cy.get('#login-button').should('exist').click()
+
+        // Expected Result
+        cy.url().should('include', '/') 
+        cy.get('.error-message-container').should('exist').and('contain.text', 'Username and password do not match any user in this service') 
+
+        // Audit
+        cy.screenshot('TC-AU-004 - Expected Result')
+    })
+})
+  
